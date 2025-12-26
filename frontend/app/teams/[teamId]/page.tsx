026f75d610f6e068f823f9799f7500891ffcc982
Tyler Daw <tylerdaw87@outlook.com>
2025-12-25 21:08:35 -0600

    Added page routing to teams depending on which logo is pressed. Ran into issue where the Prop will not grab the teamID so when the page is loaded it shows no name for the team.

--- frontend/app/teams/[teamId]/page.tsx
+++ frontend/app/teams/[teamId]/page.tsx
@@ -1,12 +1,15 @@
+
 type Props = {
   params: { teamId: string };
 };
 
 export default function TeamPage({ params }: Props) {
+  console.log(params.teamId)
   return (
     <main style={{ padding: 24 }}>
       <h1>Team: {params.teamId}</h1>
       <p>Team analytics coming soon.</p>
+      
     </main>
   );
 }
--- frontend/components/SwipeCarousel/SwipeCarousel.tsx
+++ frontend/components/SwipeCarousel/SwipeCarousel.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import styles from './SwipeCarousel.module.css';
+import { useRouter } from "next/navigation";
+
 
 
 type Team = {
@@ -11,9 +13,12 @@ type Team = {
     image_url: string;
 }
 //TODO: Make function that will call the embla API and based on which index is centered in the viewpoint, have index + 1 and index - 1 scale and animate to get bigger once it goes more towards the center?
+
+
 export default function SwipeCarousel() {
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, skipSnaps: true });
     const [teams, setTeams] = useState<Team[]>([]);
+    const router = useRouter();
 
     useEffect(() => {
         fetch('http://localhost:8000/teams')
@@ -33,8 +38,11 @@ export default function SwipeCarousel() {
         <div className = {styles.embla} ref={emblaRef}>
             <div className={styles.embla__container}>
                 {teams.map((team, index) => (
-                    <div className = {styles.embla__slide} key = {index}>
+                    <div className = {styles.embla__slide} key = {index} onClick = {() => {
+                        router.push(`teams/${team.team_id}`)
+                    }}>
                         <img src = {team.image_url} alt = {team.team_name}></img>
+                        
                     </div>
                 ))}
 
